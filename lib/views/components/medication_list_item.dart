eb1b711de2b2c3a1054e66977ca7d796a4513a20
c-info <crystalinfoway11@gmail.com>
2024-01-05 15:56:15 +0530

    pet medication complete

--- lib/views/components/medication_list_item.dart
+++ lib/views/components/medication_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:pet_shouq/views/components/components.dart';
 
